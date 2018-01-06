41d9215af4faa5b5a0fa9f6c9467c5aa6afd2225
misaelferrer <35177416+misaelferrer@users.noreply.github.com>
2018-01-06 18:20:51 -0400

    Add files via upload

--- condominio/urls.py
+++ condominio/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from censo import views
 
 urlpatterns = [
+	path('', views.index, name='index'),
 	path('censo/', include('censo.urls')),
     path('admin/', admin.site.urls),
 ]
