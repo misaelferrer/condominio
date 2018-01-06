4c676b280f7c84b3bfa27e019d2a957ac162e320
misaelferrer <35177416+misaelferrer@users.noreply.github.com>
2018-01-06 18:04:41 -0400

    Update settings.py

--- condominio/settings.py
+++ condominio/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 's(oes6y(kf4&3u=mudku4@q(7x44ba4$c__r+-px-nk3&0)!!3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
