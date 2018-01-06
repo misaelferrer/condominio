42203477cf3777deb770b482567a5c45ef35e19b
misaelferrer <35177416+misaelferrer@users.noreply.github.com>
2018-01-06 19:07:58 -0400

    Add files via upload

--- condominio/wsgi.py
+++ condominio/wsgi.py
@@ -12,5 +12,9 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "condominio.settings")
+from dj_static import Cling
 
-application = get_wsgi_application()
+
+application = Cling(get_wsgi_application())
+
+ 
