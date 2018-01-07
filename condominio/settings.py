54b6d7385b2c009ffa5c64fb769cd461f4554fd7
misaelferrer <35177416+misaelferrer@users.noreply.github.com>
2018-01-07 11:20:07 -0400

    Update settings.py

--- condominio/settings.py
+++ condominio/settings.py
@@ -36,7 +36,7 @@ ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'censo.apps.CensoConfig',
-    'bootstrap_admin',
+    'suit',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
