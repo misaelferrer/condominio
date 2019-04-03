0b10c18c2ccac9b2e09432c5a1179b321781ff56
misaelferrer <35177416+misaelferrer@users.noreply.github.com>
2019-04-03 16:22:16 -0500

    actualizando la pagina
    actualizando la pagina

--- condominio/settings.py
+++ condominio/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'votacion',
     'finanzas',
     'guardias',
+    'web',
     'jet',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -132,5 +133,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
--- condominio/urls.py
+++ condominio/urls.py
@@ -15,8 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from censo import views
-
+from web import views
 urlpatterns = [
 	path('', views.index, name='index'),
 	path('censo/', include('censo.urls')),
